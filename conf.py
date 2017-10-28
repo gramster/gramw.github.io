40c899c2096b6503cb5b25a3af868fd597274cbd
geekraver <gram@geekraver.com>
2017-10-27 19:53:48 -0700

    Nikola auto commit.
    Nikola version: 7.8.10

--- conf.py
+++ conf.py
@@ -1053,7 +1053,6 @@ LOGGING_HANDLERS = {
 
 # Put in global_context things you want available on all your templates.
 # It can be anything, data, functions, modules, etc.
-GLOBAL_CONTEXT = {}
 
 # Add functions here and they will be called with template
 # GLOBAL_CONTEXT as parameter when the template is about to be
